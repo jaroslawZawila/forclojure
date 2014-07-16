5bcb8f8c2fd351f6b6fd868017ecd2a3df196fe8
Jaroslaw Zawila <jaroslaw.zawila@microsoft.com>
2014-07-16 20:06:46 +0100

    final solution for 67

--- src/forclojure/problem_67.clj
+++ src/forclojure/problem_67.clj
@@ -1,26 +1,11 @@
 (ns forclojure.problem-67)
 
-;need a seq wich contains prime numbers
-(defn ft [x]
-  (take x (filter number?
-                  (map #(fn [n]
-                         (when-not (= 1 n)
-                           (if (= 1 (count (filter #(fn [r] (= 0 r)) (map #(fn [s] (rem n s)) (range 1 (+ 1 (Math/round (Math/sqrt n))))))))
-                             n false)))
-                       (range)))))
-
-
-
-
 (defn ft [n]
-  (when-not (= 1 n)
-    (if (= 1 (count (filter #(= 0 %) (map #(rem n %) (range 1 (+ 1 (Math/round (Math/sqrt n))))))))
-      n false)))
-
-(take 100 (filter number? (map #(ft %) (range))))
-
-(ft 11)
-
+  (take n
+        (remove
+          (fn [x]
+            (some #(= 0 (rem x %)) (range 2 (inc (Math/round (Math/sqrt x))))))
+          (iterate inc 2))))
 
 (= (ft 2) [2 3])
 (= (ft 5) [2 3 5 7 11])
